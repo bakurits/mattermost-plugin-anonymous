f2082935e7b8e7ce6459e49fe2789e4f2a9f402d
CSenshi <saba.pochkhua@gmail.com>
2020-05-03 22:58:31 +0400

    change naming from destination to package

--- mocker.sh
+++ mocker.sh
@@ -3,8 +3,8 @@
 # Github Repository Link
 GH_REPO=github.com/bakurits/mattermost-plugin-anonymous
 
-# Array of (destination,interface) tuples
-# If new element needs to be added, please delimit destination and interface by comma like other elements
+# Array of (package,interface) tuples
+# If new element needs to be added, please delimit package and interface by comma like other elements
 array=(
     anonymous,Anonymous
     command,Handler
@@ -17,12 +17,12 @@ array=(
 for i in "${array[@]}"; do IFS=",";
     set -- ${i};
 
-    DESTINATION=server/${1}
+    PACKAGE=server/${1}
     INTERFACE=${2}
     MOCK_FILE=$(echo ${INTERFACE} | awk '{print tolower($0)}')_mock.go
 
-    echo "Generating Mock for Interaface ${INTERFACE} in /${DESTINATION}..."
+    echo "Generating Mock for Interaface ${INTERFACE} in /${PACKAGE}..."
 
     # Actual Command
-    mockgen -destination=${DESTINATION}/mock/${MOCK_FILE} -package=mock ${GH_REPO}/${DESTINATION} ${INTERFACE}
+    mockgen -destination=${PACKAGE}/mock/${MOCK_FILE} -package=mock ${GH_REPO}/${PACKAGE} ${INTERFACE}
 done
