329ee1eda6fd04e5dc529a861d3b6a7360140854
Saba Pochkhua <saba.pochkhua@gmail.com>
2020-05-03 23:05:36 +0400

    Automatic mock files generator (#29)
    * add array structure and iteration
    
    * add mock file variable generation from interface
    
    * add generating mocks
    
    * add mocker to make
    
    * change naming from destination to package
    
    * change delimiter from comma to colon

---
+++ mocker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Github Repository Link
+GH_REPO=github.com/bakurits/mattermost-plugin-anonymous
+
+# Array of (package,interface) tuples
+# If new element needs to be added, please delimit package and interface by colon like other elements
+array=(
+    anonymous:Anonymous
+    command:Handler
+    plugin:Plugin
+    store:Store
+    utils/store:KVStore
+)
+
+# Iterate over each item in array and generate mock
+for i in "${array[@]}"; do IFS=":";
+    set -- ${i};
+
+    PACKAGE=server/${1}
+    INTERFACE=${2}
+    MOCK_FILE=$(echo ${INTERFACE} | awk '{print tolower($0)}')_mock.go
+
+    echo "Generating Mock for Interaface ${INTERFACE} in /${PACKAGE}..."
+
+    # Actual Command
+    mockgen -destination=${PACKAGE}/mock/${MOCK_FILE} -package=mock ${GH_REPO}/${PACKAGE} ${INTERFACE}
+done
