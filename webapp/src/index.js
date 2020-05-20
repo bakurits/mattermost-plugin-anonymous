16b857a0ef362920aacdd8dfdfca9b0bd63e95f8
dbejanishvili <datobejanishvili@gmail.com>
2020-05-21 01:35:43 +0400

    added encryption icon

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,23 @@
+import React from 'react';
+
 import manifest from './manifest';
+
 import Hooks from './hook/hook';
+import Icon from './components/icon';
 
 export default class Plugin {
     initialize(registry, store) {
         const hook = new Hooks(store, null);
+        registry.registerChannelHeaderButtonAction(
+            // eslint-disable-next-line react/jsx-filename-extension
+            <Icon/>,
+            (channel) => {
+                // eslint-disable-next-line no-console
+                console.log(channel);
+            },
+            'toggle encryption',
+            'toggle encryption'
+        );
         registry.registerSlashCommandWillBePostedHook(hook.slashCommandWillBePostedHook);
         registry.registerMessageWillFormatHook(hook.messageWillFormatHook);
     }
