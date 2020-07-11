47033cdb5ced76f4d779d0cb0a2bd3be9368cf32
bakurits <bakuricucxashvili@gmail.com>
2020-07-12 00:48:54 +0400

    added aes encoder/decoder

--- webapp/tests/test.js
+++ webapp/tests/test.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-magic-numbers,max-nested-callbacks */
 import 'mattermost-webapp/tests/setup';
 import {Key} from '../src/encrypt/key';
+import {decrypt, encrypt} from '../src/encrypt/aes';
 
-import {
-    generateKeyPair, loadKeyFromLocalStorage,
-    storePrivateKey,
-} from '../src/encrypt/key_manager';
+import {generateKeyPair, loadKeyFromLocalStorage, storePrivateKey} from '../src/encrypt/key_manager';
 
 test('should be decrypted same', () => {
     const key = generateKeyPair();
@@ -84,5 +82,14 @@ test('should not be decrypted with public key', () => {
         const dec = keyPublic.decrypt(enc);
 
         expect(dec).toEqual(null);
+    });
+});
+
+test('aes tests', () => {
+    const testsInput = ['[1, 3, 123]', '{key: \'value\'}', 'bakuri'];
+    testsInput.forEach((test) => {
+        const data = encrypt(test);
+        const g = decrypt(data.message, data.key);
+        expect(g).toEqual(test);
     });
 });
